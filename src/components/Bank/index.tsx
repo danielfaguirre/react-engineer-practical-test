d7c19204a4cbe56f94ac595dd43d033991b84a86
Daniel <danielfernaco@gmail.com>
2023-04-20 08:52:16 -0500

    Added Mui

--- src/components/Bank/index.tsx
+++ src/components/Bank/index.tsx
@@ -1,16 +1,23 @@
+import { Card, CardContent, Typography } from "@mui/material"
 import { BankType } from "../../components/Bank/model"
+import style from './styles.module.css'
 
 export type BankProps = {
   bank: BankType
+  onClick: (item: BankType) => void
 }
 
-const Bank = ({ bank }: BankProps) => {
+const Bank = ({ bank, onClick }: BankProps) => {
   const { bankName, description, age } = bank
   return (
-    <div>
-      <h1>{bankName}</h1>
-      <p>{description}</p>
-      <small>{age}</small>
+    <div className={style.bankContainer}>
+      <Card className={style.bankCard} onClick={() => onClick(bank)}>
+        <CardContent>
+          <Typography className={style.title} variant="h5">{bankName}</Typography>
+          <Typography variant="body1">{description}</Typography>
+          <Typography variant="caption">Age: {age}</Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
--- src/modules/BankList/BankListView.tsx
+++ src/modules/BankList/BankListView.tsx
@@ -1,10 +1,23 @@
+import { Grid, Typography } from '@mui/material';
 import Bank from "../../components/Bank"
 import { BankListViewProps } from "./models"
+import { BankType } from "../../components/Bank/model"
+import style from './styles.module.css'
 
 const BankListView = ({ banks }: BankListViewProps) => {
+  const handleClick = ({ url }: BankType) => {
+    window.open(url, '_blank')
+  }
   return (
-    <div>
-      {banks.map((bank) => <Bank key={bank.bankName} bank={bank} />)}
+    <div className={style.BankListContainer}>
+      <Typography className={style.title} variant='h4'>React Engineer Practical Test</Typography>
+      <Grid container>
+        {banks.map((bank) => (
+          <Grid key={bank.bankName} xs={12} sm={6} lg={4}>
+            <Bank onClick={handleClick} bank={bank} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
